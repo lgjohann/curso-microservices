669056b05be42fda76071dfee3d7f9dfbcd234e6
Guilherme Johann <guilherme4695@gmail.com>
2025-01-07 16:28:56 -0300

    Configuração inicial do projeto

---
+++ eurekaserver/src/test/java/com/johann/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johann.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
