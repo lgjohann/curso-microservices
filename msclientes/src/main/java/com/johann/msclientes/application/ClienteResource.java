2159510f24eb33995c95bbc0c15652bf89f4081d
Guilherme Johann <guilherme4695@gmail.com>
2025-01-07 17:54:11 -0300

    mapeamento do msclientes pro eurekaserver, inicialização dos arquivos de controller e servicos

---
+++ msclientes/src/main/java/com/johann/msclientes/application/ClienteResource.java
@@ -0,0 +1,15 @@
+package com.johann.msclientes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteResource {
+
+    @GetMapping
+    public String status() {
+        return "ok";
+    }
+}
