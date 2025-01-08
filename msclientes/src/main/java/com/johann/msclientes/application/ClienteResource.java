49f8feea22b0f384b4c28da73aeee4a0385e3f2a
Guilherme Johann <guilherme4695@gmail.com>
2025-01-08 13:27:33 -0300

    Criação de alguns endpoints e criação do mscloudgateway

--- msclientes/src/main/java/com/johann/msclientes/application/ClienteResource.java
+++ msclientes/src/main/java/com/johann/msclientes/application/ClienteResource.java
@@ -1,15 +1,45 @@
 package com.johann.msclientes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.johann.msclientes.application.representation.ClienteSaveRequest;
+import com.johann.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClienteResource {
+
+    private final ClienteService clienteService;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> ClienteSaveRequest(@RequestBody ClienteSaveRequest request) {
+        Cliente cliente = request.toModel();
+        clienteService.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public  ResponseEntity<Cliente> dadosCliente(@RequestParam String cpf) {
+        Optional<Cliente> cliente = clienteService.getByCPF(cpf);
+        if(cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente.get());
     }
 }
